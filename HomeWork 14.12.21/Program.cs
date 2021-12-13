aa12cda6cc14bf736feb70c9fc2a9a09457b396a
Mayil Kerimov <mayilik@code.edu.az>
2021-12-14 01:32:12 +0400

    hi

---
+++ HomeWork 14.12.21/Program.cs
@@ -0,0 +1,19 @@
+﻿using Library;
+using System;
+
+namespace HomeWork_14._12._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator Calculate = new Calculator();
+            Calculate.a = 8;
+            Calculate.b = 10;
+            Calculate.Multiply();
+            Calculate.Plus();
+            Calculate.Minus();
+            Calculate.Divide();
+        }
+    }
+}
---
+++ Library/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Library
+{
+    public class Calculator
+    {
+        public double a;
+        public double b;
+        public double res;
+        public void Plus()
+        {
+            res = a + b;
+            Result();
+        }
+        public void Minus()
+        {
+             res = a - b;
+            Result();
+        }
+        public void Multiply()
+        {
+            res = a * b;
+            Result();
+        }
+        public void Divide()
+        {
+            res = a / b;
+            Result();
+        }
+        public void Result()
+        {
+            Console.WriteLine(res);
+        }
+    }
+}
